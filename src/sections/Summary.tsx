952123c63a7098a406ab9a7d55b124176b7eecae
Nuruddin <104330069+Xivlon@users.noreply.github.com>
2026-08-13 14:47:58 +0300

    Refactor Summary component to streamline return
    Removed unnecessary return statement in Summary component.

--- src/sections/Summary.tsx
+++ src/sections/Summary.tsx
@@ -37,8 +37,6 @@ export default function Summary() {
         </div>
       </div>
     </div>
-  );
-  return (
     <div>
       <div className="grid gap-px border border-border bg-border">
         <a
